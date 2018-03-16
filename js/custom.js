9e87aa22dbcf41152eeebc74397ced5e60bfc84b
Joshua Crown <joshuacrown@gmail.com>
2018-03-16 16:31:59 -0700

    fixed bug on nav bar

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,15 @@
 $('#navigation a').click(function () {
-    $('#navigation').removeClass('collapse');
-    $('#navigation').removeClass('in');
-    $('#navigation').addClass('collapsing');
-    $('#navigation').attr('aria-expanded', 'false');
-    setTimeout(function () {
-        $('#navigation').removeClass('collapsing');
-        $('#navigation').addClass('collapse');
-        $('#navigation').attr('aria-expanded', 'true');
-    }, 300);
+    var h = window.innerWidth;
+    console.log(h < 768);
+    if (h < 768) {
+        $('#navigation').removeClass('collapse');
+        $('#navigation').removeClass('in');
+        $('#navigation').addClass('collapsing');
+        $('#navigation').attr('aria-expanded', 'false');
+        setTimeout(function () {
+            $('#navigation').removeClass('collapsing');
+            $('#navigation').addClass('collapse');
+            $('#navigation').attr('aria-expanded', 'true');
+        }, 300);
+    }
 });
